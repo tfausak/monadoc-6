eff85ae29fc6d5b44e9573dba39521a57fd8af33
Taylor Fausak <taylor@fausak.me>
2021-05-16 22:29:25 -0400

    Extract function arguments into top level values

--- src/lib/Monadoc/Type/Context.hs
+++ src/lib/Monadoc/Type/Context.hs
@@ -1,6 +1,7 @@
 module Monadoc.Type.Context where
 
 import qualified Data.Pool as Pool
+import qualified Data.Time as Time
 import qualified Database.SQLite.Simple as Sql
 import qualified Monadoc.Type.Config as Config
 
@@ -14,7 +15,16 @@ fromConfig c = do
     p <- Pool.createPool
         (Sql.open $ Config.database c)
         Sql.close
-        1 -- stripe count
-        60 -- seconds
-        16 -- resource count
+        stripeCount
+        idleTime
+        resourceCount
     pure Context { config = c, pool = p }
+
+stripeCount :: Int
+stripeCount = 1
+
+idleTime :: Time.NominalDiffTime
+idleTime = 60
+
+resourceCount :: Int
+resourceCount = 16
