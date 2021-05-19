13d7d40ea5ce0917e349d347bf356328437063af
Taylor Fausak <taylor@fausak.me>
2021-05-19 02:07:59 +0000

    Add an HTTPS manager to the context

--- src/lib/Monadoc/Type/Context.hs
+++ src/lib/Monadoc/Type/Context.hs
@@ -4,21 +4,25 @@ import qualified Data.Pool as Pool
 import qualified Data.Time as Time
 import qualified Database.SQLite.Simple as Sql
 import qualified Monadoc.Type.Config as Config
+import qualified Network.HTTP.Client as Client
+import qualified Network.HTTP.Client.TLS as Tls
 
 data Context = Context
     { config :: Config.Config
+    , manager :: Client.Manager
     , pool :: Pool.Pool Sql.Connection
-    } deriving Show
+    }
 
 fromConfig :: Config.Config -> IO Context
 fromConfig c = do
+    m <- Tls.newTlsManager
     p <- Pool.createPool
         (Sql.open $ Config.database c)
         Sql.close
         stripeCount
         idleTime
         resourceCount
-    pure Context { config = c, pool = p }
+    pure Context { config = c, manager = m, pool = p }
 
 stripeCount :: Int
 stripeCount = 1
