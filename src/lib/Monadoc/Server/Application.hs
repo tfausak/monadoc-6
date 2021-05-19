41cdb4b43ab086b82debdbca576ae2008182c9e3
Taylor Fausak <taylor@fausak.me>
2021-05-19 11:30:33 +0000

    Add a favicon

--- src/lib/Monadoc/Server/Application.hs
+++ src/lib/Monadoc/Server/Application.hs
@@ -25,6 +25,49 @@ application request respond = do
                 Map.empty
                 [])
             []
+        ("GET", ["favicon.ico"]) -> respond $ Response.status Http.found302
+            [ (Http.hLocation, Convert.stringToUtf8 "monadoc.svg")
+            ]
+        ("GET", ["monadoc.svg"]) -> respond . Response.xml Http.ok200 [] $ Xml.Document
+            (Xml.Prologue [] Nothing [])
+            (Xml.Element
+                (Xml.Name (Convert.stringToText "svg") Nothing Nothing)
+                (Map.fromList
+                    [ (Xml.Name (Convert.stringToText "xmlns") Nothing Nothing, Convert.stringToText "http://www.w3.org/2000/svg")
+                    , (Xml.Name (Convert.stringToText "viewBox") Nothing Nothing, Convert.stringToText "0 0 16 16")
+                    ])
+                [ Xml.NodeElement $ Xml.Element
+                    (Xml.Name (Convert.stringToText "g") Nothing Nothing)
+                    (Map.fromList
+                        [ (Xml.Name (Convert.stringToText "fill") Nothing Nothing, Convert.stringToText "#5e2ca5")
+                        ])
+                    [ Xml.NodeElement $ Xml.Element
+                        (Xml.Name (Convert.stringToText "path") Nothing Nothing)
+                        (Map.fromList
+                            [ (Xml.Name (Convert.stringToText "d") Nothing Nothing, Convert.stringToText "M 2 7 h 2 v 7 h -2 z")
+                            ])
+                        []
+                    , Xml.NodeElement $ Xml.Element
+                        (Xml.Name (Convert.stringToText "path") Nothing Nothing)
+                        (Map.fromList
+                            [ (Xml.Name (Convert.stringToText "d") Nothing Nothing, Convert.stringToText "M 12 7 h 2 v 7 h -2 z")
+                            ])
+                        []
+                    , Xml.NodeElement $ Xml.Element
+                        (Xml.Name (Convert.stringToText "path") Nothing Nothing)
+                        (Map.fromList
+                            [ (Xml.Name (Convert.stringToText "d") Nothing Nothing, Convert.stringToText "M 2 5 l 6 2 l 6 -2 v 2 l -6 2 l -6 -2 z")
+                            ])
+                        []
+                    , Xml.NodeElement $ Xml.Element
+                        (Xml.Name (Convert.stringToText "path") Nothing Nothing)
+                        (Map.fromList
+                            [ (Xml.Name (Convert.stringToText "d") Nothing Nothing, Convert.stringToText "M 2 2 l 6 2 l 6 -2 v 2 l -6 2 l -6 -2 z")
+                            ])
+                        []
+                    ]
+                ])
+            []
         ("GET", ["monadoc.xsl"]) -> respond . Response.xml Http.ok200 [] $ Xml.Document
             (Xml.Prologue [] Nothing [])
             (Xml.Element
@@ -62,6 +105,14 @@ application request respond = do
                                 (Xml.Name (Convert.stringToText "title") Nothing Nothing)
                                 Map.empty
                                 [Xml.NodeContent $ Convert.stringToText "Monadoc"]
+                            , Xml.NodeElement $ Xml.Element
+                                (Xml.Name (Convert.stringToText "link") Nothing Nothing)
+                                (Map.fromList
+                                    [ (Xml.Name (Convert.stringToText "rel") Nothing Nothing, Convert.stringToText "icon")
+                                    , (Xml.Name (Convert.stringToText "type") Nothing Nothing, Convert.stringToText "image/svg+xml")
+                                    , (Xml.Name (Convert.stringToText "href") Nothing Nothing, Convert.stringToText "monadoc.svg")
+                                    ])
+                                []
                             ]
                         , Xml.NodeElement $ Xml.Element
                             (Xml.Name (Convert.stringToText "body") Nothing Nothing)
