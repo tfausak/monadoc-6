ee5731f04d0a9d1930ac539129ec5aa81631f8a9
Taylor Fausak <taylor@fausak.me>
2021-05-29 15:57:40 +0000

    Upgrade Witch

--- src/lib/Monadoc/Prelude/Orphanage.hs
+++ src/lib/Monadoc/Prelude/Orphanage.hs
@@ -2,22 +2,15 @@
 
 module Monadoc.Prelude.Orphanage where
 
-import qualified Data.ByteString
 import qualified Data.Text
 import qualified Database.SQLite.Simple
 import qualified Prelude
 import qualified Witch
 
-instance Witch.TryFrom Data.ByteString.ByteString Prelude.String where
-    tryFrom = Witch.eitherTryFrom (\ s -> Prelude.fmap (Witch.into @Prelude.String) (Witch.tryInto @Data.Text.Text s))
-
 instance Witch.From Database.SQLite.Simple.Query Prelude.String where
     from = Witch.via @Data.Text.Text
 
 instance Witch.From Database.SQLite.Simple.Query Data.Text.Text
-
-instance Witch.From Prelude.String Data.ByteString.ByteString where
-    from = Witch.via @Data.Text.Text
 
 instance Witch.From Prelude.String Database.SQLite.Simple.Query where
     from = Witch.via @Data.Text.Text
