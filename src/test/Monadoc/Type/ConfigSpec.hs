e657acd295f788cbde252d648cfbf46a1c1ae28a
Taylor Fausak <taylor@fausak.me>
2021-05-29 17:37:12 +0000

    Fix the tests

--- src/test/Monadoc/Type/ConfigSpec.hs
+++ src/test/Monadoc/Type/ConfigSpec.hs
@@ -24,27 +24,27 @@ spec = do
             fmap fst (Config.fromArguments ["--a=0"]) `Hspec.shouldBe` Just [Warning.UnrecognizedOption "--a=0"]
 
         Hspec.it "sets the help option" <| do
-            fmap (Config.help . snd) (Config.fromArguments ["-h"]) `Hspec.shouldBe` Just True
-            fmap (Config.help . snd) (Config.fromArguments ["-?"]) `Hspec.shouldBe` Just True
-            fmap (Config.help . snd) (Config.fromArguments ["--help"]) `Hspec.shouldBe` Just True
+            fmap (Config.help <. snd) (Config.fromArguments ["-h"]) `Hspec.shouldBe` Just True
+            fmap (Config.help <. snd) (Config.fromArguments ["-?"]) `Hspec.shouldBe` Just True
+            fmap (Config.help <. snd) (Config.fromArguments ["--help"]) `Hspec.shouldBe` Just True
 
         Hspec.it "rejects passing an argument to an option that doesn't accept one" <| do
             Config.fromArguments ["--help=0"] `Hspec.shouldBe` Nothing
 
         Hspec.it "sets the version option" <| do
-            fmap (Config.version . snd) (Config.fromArguments ["--version"]) `Hspec.shouldBe` Just True
+            fmap (Config.version <. snd) (Config.fromArguments ["--version"]) `Hspec.shouldBe` Just True
 
         Hspec.it "sets the host option" <| do
-            fmap (Config.host . snd) (Config.fromArguments ["--host=0"]) `Hspec.shouldBe` Just (String.fromString "0")
+            fmap (Config.host <. snd) (Config.fromArguments ["--host=0"]) `Hspec.shouldBe` Just (String.fromString "0")
 
         Hspec.it "rejects not passing an argument to an option that requires one" <| do
             Config.fromArguments ["--host"] `Hspec.shouldBe` Nothing
 
         Hspec.it "sets the port option" <| do
-            fmap (Config.port . snd) (Config.fromArguments ["--port=0"]) `Hspec.shouldBe` Just 0
+            fmap (Config.port <. snd) (Config.fromArguments ["--port=0"]) `Hspec.shouldBe` Just 0
 
         Hspec.it "rejects an invalid port" <| do
             Config.fromArguments ["--port=x"] `Hspec.shouldBe` Nothing
 
         Hspec.it "sets the database option" <| do
-            fmap (Config.database . snd) (Config.fromArguments ["--database=0"]) `Hspec.shouldBe` Just "0"
+            fmap (Config.database <. snd) (Config.fromArguments ["--database=0"]) `Hspec.shouldBe` Just "0"
