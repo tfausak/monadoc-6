c21fe116c6c113a1ea09ee4f879accec05ced4f6
Taylor Fausak <taylor@fausak.me>
2021-06-02 21:29:29 -0400

    Fix infinite loop

--- src/lib/Monadoc/Type/PackageName.hs
+++ src/lib/Monadoc/Type/PackageName.hs
@@ -6,16 +6,19 @@ import qualified Distribution.Parsec as Cabal
 import qualified Distribution.Types.PackageName as Cabal
 
 newtype PackageName
-    = PackageName String
+    = PackageName Cabal.PackageName
     deriving (Eq, Ord, Show)
 
 instance TryFrom String PackageName where
-    tryFrom = maybeTryFrom <| Cabal.simpleParsec .> fmap (from @Cabal.PackageName)
+    tryFrom = maybeTryFrom <| \ s -> case Cabal.simpleParsec s of
+        Nothing -> Nothing
+        Just n -> Just <| from @Cabal.PackageName n
 
-instance From PackageName String
+instance From PackageName String where
+    from = into @Cabal.PackageName .> Cabal.unPackageName
 
 instance From Cabal.PackageName PackageName where
-    from = Cabal.unPackageName .> unsafeInto @PackageName
+    from = PackageName
 
 instance From PackageName Cabal.PackageName where
-    from = into @String .> Cabal.mkPackageName
+    from (PackageName x) = x
