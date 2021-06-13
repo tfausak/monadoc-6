d61fe4bcedbb154c024036280e12cb998eaa9c6b
Taylor Fausak <taylor@fausak.me>
2021-06-13 16:25:24 -0400

    Relax parsing of component names

--- src/lib/Monadoc/Type/ComponentName.hs
+++ src/lib/Monadoc/Type/ComponentName.hs
@@ -4,7 +4,6 @@ import Monadoc.Prelude
 
 import qualified Database.SQLite.Simple.FromField as Sql
 import qualified Database.SQLite.Simple.ToField as Sql
-import qualified Distribution.Parsec as Cabal
 import qualified Distribution.Types.PackageName as Cabal
 import qualified Distribution.Types.UnqualComponentName as Cabal
 import qualified Monadoc.Class.ToXml as ToXml
@@ -19,8 +18,13 @@ instance From Cabal.UnqualComponentName ComponentName
 
 instance From ComponentName Cabal.UnqualComponentName
 
+-- | Note that this /should/ use the @Parsec@ instance of
+-- 'Cabal.UnqualComponentName', but it doesn't seem to work properly. See
+-- <https://github.com/haskell/cabal/issues/7441>.
 instance TryFrom String ComponentName where
-    tryFrom = eitherTryFrom $ bimap userError (from @Cabal.UnqualComponentName) . Cabal.eitherParsec
+    tryFrom = maybeTryFrom $ \ string -> do
+        guard . not $ null string
+        pure . from @Cabal.UnqualComponentName $ Cabal.mkUnqualComponentName string
 
 instance From ComponentName String where
     from = Cabal.unUnqualComponentName . into @Cabal.UnqualComponentName
@@ -34,6 +38,8 @@ instance Sql.ToField ComponentName where
 instance ToXml.ToXml ComponentName where
     toXml = ToXml.toXml . into @String
 
+-- | The 'Cabal.packageNameToUnqualComponentName' function might go away. See
+-- <https://github.com/haskell/cabal/issues/5811>.
 instance From PackageName.PackageName ComponentName where
     from = into @ComponentName . Cabal.packageNameToUnqualComponentName . into @Cabal.PackageName
 
