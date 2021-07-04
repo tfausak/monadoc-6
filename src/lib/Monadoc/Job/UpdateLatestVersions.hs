e7229a7820f4f2c4110cddc38249c5845f4ff2cd
Taylor Fausak <taylor@fausak.me>
2021-07-04 10:01:38 -0400

    Create separate jobs for processing the Hackage index

---
+++ src/lib/Monadoc/Job/UpdateLatestVersions.hs
@@ -0,0 +1,44 @@
+module Monadoc.Job.UpdateLatestVersions where
+
+import Monadoc.Prelude
+
+import qualified Control.Concurrent.STM as Stm
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Monadoc.Model.LatestVersion as LatestVersion
+import qualified Monadoc.Type.Context as Context
+import qualified Monadoc.Type.PackageName as PackageName
+import qualified Monadoc.Type.Revision as Revision
+import qualified Monadoc.Type.Version as Version
+import qualified Monadoc.Type.VersionRange as VersionRange
+import qualified Monadoc.Utility.Foldable as Foldable
+
+run
+    :: Context.Context
+    -> Stm.TVar (Map (PackageName.PackageName, Version.Version) Revision.Revision)
+    -> Stm.TVar (Map PackageName.PackageName VersionRange.VersionRange)
+    -> IO ()
+run context revisionsVar preferredVersionsVar = do
+    oldLatestVersions <- Context.withConnection context LatestVersion.selectAll
+    revisions <- Stm.atomically $ Stm.readTVar revisionsVar
+    preferredVersions <- Stm.atomically $ Stm.readTVar preferredVersionsVar
+    let
+        newLatestVersions =
+            revisions
+                & Map.mapMaybe Revision.decrement
+                & Map.toList
+                & fmap (\ ((p, v), r) ->
+                    let c = Map.findWithDefault VersionRange.any p preferredVersions
+                    in (p, [(VersionRange.contains v c, (v, r))]))
+                & Map.fromListWith (<>)
+                & Map.mapMaybe Foldable.maximum
+                & fmap snd
+    newLatestVersions
+        & Map.toList
+        & traverse_ (\ (p, (v1, r1)) -> case Map.lookup p oldLatestVersions of
+            Just (v0, r0) | v0 == v1 && r0 == r1 -> pure ()
+            _ -> Context.withConnection context $ \ connection ->
+                LatestVersion.upsert connection $ LatestVersion.new p v1 r1)
+    Set.difference (Map.keysSet oldLatestVersions) (Map.keysSet newLatestVersions)
+        & traverse_ (\ p -> Context.withConnection context $ \ connection ->
+            LatestVersion.deleteByPackage connection p)
---
+++ src/lib/Monadoc/Job/UpdatePreferredVersions.hs
@@ -0,0 +1,25 @@
+module Monadoc.Job.UpdatePreferredVersions where
+
+import Monadoc.Prelude
+
+import qualified Control.Concurrent.STM as Stm
+import qualified Data.Map as Map
+import qualified Monadoc.Model.PreferredVersions as PreferredVersions
+import qualified Monadoc.Type.Context as Context
+import qualified Monadoc.Type.PackageName as PackageName
+import qualified Monadoc.Type.VersionRange as VersionRange
+
+run
+    :: Context.Context
+    -> Stm.TVar (Map PackageName.PackageName VersionRange.VersionRange)
+    -> IO ()
+run context preferredVersionsVar = do
+    oldPreferredVersions <- Context.withConnection context PreferredVersions.selectAll
+    newPreferredVersions <- Stm.atomically $ Stm.readTVar preferredVersionsVar
+    newPreferredVersions
+        & Map.toList
+        & fmap (uncurry PreferredVersions.new)
+        & traverse_ (\ pv -> case Map.lookup (PreferredVersions.packageName pv) oldPreferredVersions of
+            Just v | v == PreferredVersions.versionRange pv -> pure ()
+            _ -> Context.withConnection context $ \ connection ->
+                PreferredVersions.upsert connection pv)
