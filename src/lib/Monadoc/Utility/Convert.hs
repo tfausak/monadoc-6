f322ecd69401f0fc4294b90f1b45a562cbe01322
Taylor Fausak <taylor@fausak.me>
2021-07-11 16:51:29 -0400

    Remove `timeToString` conversion function

--- src/lib/Monadoc/Utility/Convert.hs
+++ src/lib/Monadoc/Utility/Convert.hs
@@ -1,11 +1,7 @@
 module Monadoc.Utility.Convert where
 
 import qualified Data.String as String
-import qualified Data.Time as Time
 import qualified Network.Wai.Handler.Warp as Warp
 
 stringToHost :: String -> Warp.HostPreference
 stringToHost = String.fromString
-
-timeToString :: Time.UTCTime -> String
-timeToString = Time.formatTime Time.defaultTimeLocale "%Y-%m-%dT%H:%M:%S%3QZ"
--- src/lib/Monadoc/Utility/Log.hs
+++ src/lib/Monadoc/Utility/Log.hs
@@ -3,7 +3,6 @@ module Monadoc.Utility.Log where
 import qualified Control.Concurrent.STM as Stm
 import qualified Control.Monad.Catch as Exception
 import qualified Data.Time as Time
-import qualified Monadoc.Utility.Convert as Convert
 import qualified System.IO as IO
 import qualified System.IO.Unsafe as Unsafe
 
@@ -16,7 +15,8 @@ warn = onHandle IO.stderr
 onHandle :: IO.Handle -> String -> IO ()
 onHandle h message = do
     now <- Time.getCurrentTime
-    withLock . IO.hPutStrLn h $ Convert.timeToString now <> " " <> message
+    let timestamp = Time.formatTime Time.defaultTimeLocale "%Y-%m-%dT%H:%M:%S%3QZ" now
+    withLock . IO.hPutStrLn h $ timestamp <> " " <> message
 
 withLock :: IO a -> IO a
 withLock = Exception.bracket
